c94ddf0c5ea89a7eef314121d0685bf7290d2d46
jonathangean <jonathangean@gmail.com>
2014-07-24 15:09:22 -0400

    edit resume, add 3rd project

--- scripts.js
+++ scripts.js
@@ -1,41 +1,17 @@
-// $(function(){
-
-//   $("#show-projects").click(function(){
-//     $("#project")
-//     .html("<h3 id='projects-shown'>Projects</h3><ul><li>projeect 1</li>")});
-
-// });
-
-//   $("#projects-shown").click(function(){
-//     $("#project")
-//     .html("<h3 id='show-projects'>Projects</h3>")});
 
 $(function(){
 
-  // $("#projects").click(function(){
-  //   $("#projects-list").toggle()
-  // });
-
-
-  // $("#projects-list").toggle()
   $('#text-picture').hide();
   $('#sound-picture').hide();
-
+  $('#click-picture').hide();
 
   $("#contact-h3").click(function(){
     $("#contacts").toggle()
   });
 
-  $("#show-resume").click(function(){
-    $("#resume").toggle()
-  });
-
-  $("#resume").toggle()
-
   $("#contacts").toggle()
 
   $("#show-text-generator-picture").mouseover(function(){
-    $('#resume').hide();
     $('#text-picture').fadeIn(200);
   })
 
@@ -44,7 +20,6 @@ $(function(){
   })
 
    $("#show-twixing-board-picture").mouseover(function(){
-    $('#resume').hide();
     $('#sound-picture').fadeIn(200);
   })
 
@@ -52,4 +27,11 @@ $(function(){
     $('#sound-picture').hide();
   })
 
+$("#show-click-picture").mouseover(function(){
+    $('#click-picture').fadeIn(200);
+  })
+
+   $("#show-click-picture").mouseout(function(){
+    $('#click-picture').hide();
+  })
 });
